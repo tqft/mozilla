441c79eb255cb50b590fc53ff88baf6a08bee61e
ian <ian@tqft.gateway>
2023-06-07 09:39:37 +1000

    change thunderbird version string to be mail specific

--- tbbuild.sh
+++ tbbuild.sh
@@ -6,6 +6,8 @@ time ./mach build
 time ./mach package
 cd ..
 rm -rf ../tb/thunderbird/*
-versionstring=$(<mozilla-central/browser/config/version.txt)
+#versionstring=$(<mozilla-central/browser/config/version.txt)
+#change for thunderbird
+versionstring=$(<mozilla-central/comm/mail/config/version.txt)
 tar xvf ./mozilla-central/obj-x86_64-pc-linux-gnu/tb/dist/thunderbird-$versionstring.en-US.linux-x86_64.tar.bz2 -C ../tb/thunderbird/
 \cp -f ./mozilla-central/obj-x86_64-pc-linux-gnu/tb/dist/thunderbird-$versionstring.en-US.linux-x86_64.tar.bz2  mozExeBackup/.
